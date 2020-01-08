f8f67fd48d7840fa70081382a77c22c194ef70f7
Oisin Mulvihill <oisin.mulvihill@gmail.com>
2020-01-08 16:41:33 +0000

    correct typo

--- terraform/env.mk
+++ terraform/env.mk
@@ -3,8 +3,8 @@
 export TF_VAR_region=${REGION}
 export TF_VAR_org_id=${GCLOUD_ORG_ID}
 export TF_VAR_billing_account=${GCLOUD_BILLING_ID}
-export TF_ADMIN=${TF_ADMIN}
-export TF_CREDS=${TF_CREDS}
+export TF_ADMIN=${ORG_TF_ADMIN}
+export TF_CREDS=${ORG_TF_CREDS}
 export GOOGLE_APPLICATION_CREDENTIALS=${TF_CREDS}
 export GOOGLE_PROJECT=${TF_ADMIN}
 export SERVICE_ACCOUNT_KEY=${TF_CREDS}
